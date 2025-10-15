b385c2b1d6bc08303907b49b19bb143c891fde19
Blagoy <blagoy1234@gmail.com>
2025-10-15 09:55:15 +0300

    Initial Repo for stream decoder server

---
+++ src/test/java/com/example/video_player_be/VideoPlayerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video_player_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPlayerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
